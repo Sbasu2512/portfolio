f28a50d24ce8cfed9b803d325c42b38ce5f0579d
Sayantan Basu <81332793+Sbasu2512@users.noreply.github.com>
2025-06-03 21:39:48 +0530

    Hotfixes (#13)
    * work page responsive for tab
    
    * work page, footer done for phone view responsive
    
    * added new work exp
    
    * update to work experience
    
    * update to next js due to security vulnerability
    
    * Add new themes and fix UI
    
    * add responsiveness
    
    * edit page.js
    
    * update next js version
    
    * Revert "update next js version"
    
    This reverts commit 448f498955b4718c1e535d9237061b7c2360124f.
    
    * upgrade next js version to 15
    
    * more updates + add another theme
    
    * hotfix home errors
    
    * hotfix at home

--- src/app/Components/Home/home.js
+++ src/app/Components/Home/home.js
@@ -22,6 +22,8 @@ export default function LandingPage({ theme, windowWidth }) {
   const shouldHideContact = windowWidth < 500;
   const [index, setIndex] = useState(0);
   const [fade, setFade] = useState(true);
+   
+  useEffect(() => {
     const interval = setInterval(() => {
       setFade(false);
 
